278c43b949bd22554b1e932ad64fefdba98b075b
Satya varun <perumallavarun171@gmail.com>
2026-07-31 10:59:28 +0530

    Time: 0 ms (100%), Space: 20.5 MB (42.09%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -6,7 +6,7 @@ class Solution:
         for right in range(1,len(nums)):
             if nums[left]!=nums[right]:
                 left+=1
-            nums[left]=nums[right]
+                nums[left]=nums[right]
         return left+1
 
         
